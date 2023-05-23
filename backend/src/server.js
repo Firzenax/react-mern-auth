1c4184a853db1870d0a2427f2dfb14c0c6d245ae
Firzenax <louis.20160437@gmail.com>
2023-05-23 15:07:18 +0200

    finished project

---
+++ backend/src/server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import userRoutes from "./routes/userRoutes.js";
+import connectDB from "./config/db.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+
+dotenv.config();
+const port = process.env.PORT || 5000;
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
+app.get("/", (req, res) => res.send("API running"));
+
+app.use("/api/users", userRoutes);
+
+app.use(notFound);
+
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
